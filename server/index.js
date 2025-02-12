d60a2604ce8830bf727da23804acb2084f9833ae
varsha-2005 <varshagym123@gmail.com>
2025-02-12 20:42:20 +0530

    url

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const cors = require("cors");
 const todoRoute = require("./routes/todoRoute.js");
 const userRoute = require("./routes/userRoute.js");
 const connectDB = require("./config/db.js");
+const allowedOrigins = [process.env.APPLICATION_URL];
 
 console.log("MONGO_URI from .env:", process.env.MONGO_URI);
 const app = express();
-app.use(cors());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 
 app.use("/api/todos", todoRoute);
